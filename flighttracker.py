d0610e96e9a38f619fbcfba38472a18aa27fbfc5
Colin Hale-Brown <colinhalebrown+git@gmail.com>
2017-11-25 17:29:31 -0800

    Cleaned flighttracker.py
    I cleaned up and problem solved the flight tracker.

--- flighttracker.py
+++ flighttracker.py
@@ -4,11 +4,15 @@ import keyboard #pip install keyboard
 import threading
 from datetime import date
 from bs4 import BeautifulSoup #pip install bs4
-seconds = 0
 today = date.today()
 flightnum = raw_input('Enter airline code - flight number: ')
 seconds = input('How often do you want to check?: ')
 url = 'https://www.kayak.com/tracker/' + flightnum + '/' + str(today)
+print 'Checking for flight ' + flightnum + ' every ' + str(seconds) + ' seconds'
+#print url
+#print today
+#print flightnum
+#print seconds
 
 def track():
  if seconds > 0:
@@ -18,13 +22,14 @@ def track():
  soup = BeautifulSoup(webPage, 'html.parser')
  status_box = soup.find('div', {'class' : 'keel-grid statusSubHeadline'})
  status = status_box.text.strip()
- print status
+ flight_box = soup.find('div', {'class' : 'col col-6-12'})
+ flight = flight_box.text.strip()
+ print flight + ' ' + status
  if seconds > 0:
   while True:
    try:
     if keyboard.is_pressed('space'):
      clock.cancel()
-     print 'key pressed!'
      break
     else:
      pass
