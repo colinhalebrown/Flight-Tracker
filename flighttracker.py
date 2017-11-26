9fd5489867938f117440911d219bf158995ffc39
Colin Hale-Brown <colinhalebrown+git@gmail.com>
2017-11-25 16:47:38 -0800

    Updated flighttracker
    Added a input for how often it checks for your flight.

--- flighttracker.py
+++ flighttracker.py
@@ -2,29 +2,32 @@
 import urllib2 
 import keyboard #pip install keyboard
 import threading
+from datetime import date
 from bs4 import BeautifulSoup #pip install bs4
-flightnum = raw_input("Enter airline code - flight number: ")
-def main():
- clock = threading.Timer(2.0, main)
- clock.start()
- url = 'https://www.kayak.com/tracker/' + flightnum + '/2017-11-25'
- print url
+seconds = 0
+today = date.today()
+flightnum = raw_input('Enter airline code - flight number: ')
+seconds = input('How often do you want to check?: ')
+url = 'https://www.kayak.com/tracker/' + flightnum + '/' + str(today)
+
+def track():
+ if seconds > 0:
+  clock = threading.Timer(seconds, track)
+  clock.start()
  webPage = urllib2.urlopen(url)
- #is it a good adress, does it read?
- print "result code: " + str(webPage.getcode())
  soup = BeautifulSoup(webPage, 'html.parser')
  status_box = soup.find('div', {'class' : 'keel-grid statusSubHeadline'})
  status = status_box.text.strip()
  print status
- while True:
-  try:
-   if keyboard.is_pressed('enter'):
-    clock.cancel()
-    print 'key pressed!'
+ if seconds > 0:
+  while True:
+   try:
+    if keyboard.is_pressed('space'):
+     clock.cancel()
+     print 'key pressed!'
+     break
+    else:
+     pass
+   except:
     break
-   else:
-    pass
-  except:
-   break
-if __name__ == "__main__":
- main()
+track()
